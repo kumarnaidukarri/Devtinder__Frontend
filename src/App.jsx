8164af9d148a0cb801370acba3c530bd35710587
Kumar <kumarnaidukarri2002@gmail.com>
2026-01-08 17:07:42 +0530

    2.1 - Implemented Routing using React Router library

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router"; // react router library
+/* Declarative Mode routing:- it means we use Routing Components to configure routing. */
+
 import Home from "./components/Home.jsx";
+import Login from "./components/Login.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <>
-      <Home />
-      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
