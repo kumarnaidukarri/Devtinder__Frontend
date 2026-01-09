0f4c8aaaa6d6aa99cf5d0808b799308a103dd98e
Kumar <kumarnaidukarri2002@gmail.com>
2026-01-10 00:50:04 +0530

    3 - REDUX Store setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router"; // react router library
 /* Declarative Mode routing:- it means we use Routing Components to configure routing. */
+import { Provider } from "react-redux";
 
 import Body from "./components/Body.jsx";
 import Login from "./components/Login.jsx";
+import appStore from "./utils/store/appStore.js"; // redux store
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Body />}>
-            {/* child routes */}
-            <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Body />}>
+              {/* child routes */}
+              <Route path="/login" element={<Login />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
