0fed76018c5e042c73340ebf4f21e140a62adde5
Kumar <kumarnaidukarri2002@gmail.com>
2026-01-09 21:09:54 +0530

    2.5.1 - Login logic in react component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 const Login = () => {
+  // local state variables
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  // event handler
+  const handleLogin = async () => {
+    console.log(
+      "Make API call to backend for login(username, password). Backend will give the JWT token in cookies"
+    );
+  };
+
   return (
     <div className="card-container  flex justify-center my-10">
       {/* DaisyUI component - Card with no image */}
@@ -9,15 +22,33 @@ const Login = () => {
             {/* daisyui components - Input with fieldset and legend */}
             <fieldset className="fieldset">
               <legend className="fieldset-legend"> Email ID </legend>
-              <input type="text" className="input" placeholder="" />
+              <input
+                type="text"
+                className="input"
+                placeholder=""
+                value={emailId}
+                onChange={(event) => {
+                  setEmailId(event.target.value);
+                }}
+              />
             </fieldset>
             <fieldset className="fieldset mt-2">
               <legend className="fieldset-legend"> Password </legend>
-              <input type="text" className="input" placeholder="" />
+              <input
+                type="text"
+                className="input"
+                placeholder=""
+                value={password}
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+              />
             </fieldset>
           </div>
           <div className="card-actions justify-center mt-4">
-            <button className="btn btn-primary">Login</button>
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
           </div>
         </div>
       </div>
