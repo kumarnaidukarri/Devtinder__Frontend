05d21b2ccf9d9269c19f228feb6c3a047b8aef79
Kumar <kumarnaidukarri2002@gmail.com>
2026-01-13 01:37:05 +0530

    5.2.2 - Toast message UI show when Profile Saved

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -18,6 +18,8 @@ const EditProfile = ({ user }) => {
   const [about, setAbout] = useState(user.about);
   const [errorMessage, setErrorMessage] = useState("");
 
+  const [showToast, setShowToast] = useState(false); // update profile toast message
+
   const dispatch = useDispatch();
 
   // event handler
@@ -34,106 +36,125 @@ const EditProfile = ({ user }) => {
       const updatedUser = res?.data?.data;
 
       dispatch(addUser(updatedUser)); // dispatch an action - updates data in store
+
+      setShowToast(true); // update local state - showToast
+      setTimeout(() => {
+        // hide toast msg UI after 3 secs
+        setShowToast(false);
+      }, 3000);
     } catch (err) {
       setErrorMessage(err.response.data);
     }
   };
 
   return (
-    <div className="edit-profile-container  flex justify-center flex-wrap my-10">
-      <div className="card-container  flex justify-center mx-10">
-        {/* DaisyUI component - Card with no image */}
-        <div className="card bg-base-300 w-96 shadow-sm">
-          <div className="card-body">
-            <h2 className="card-title justify-center"> Edit Profile </h2>
-            <div>
-              {/* daisyui components - Input with fieldset and legend */}
-              <fieldset className="fieldset">
-                <legend className="fieldset-legend"> First Name: </legend>
-                <input
-                  type="text"
-                  className="input"
-                  placeholder=""
-                  value={firstName}
-                  onChange={(event) => {
-                    setFirstName(event.target.value);
-                  }}
-                />
-              </fieldset>
-              <fieldset className="fieldset mt-2">
-                <legend className="fieldset-legend"> Last Name: </legend>
-                <input
-                  type="text"
-                  className="input"
-                  placeholder=""
-                  value={lastName}
-                  onChange={(event) => {
-                    setLastName(event.target.value);
-                  }}
-                />
-              </fieldset>
-              <fieldset className="fieldset mt-2">
-                <legend className="fieldset-legend"> Photo Url: </legend>
-                <input
-                  type="text"
-                  className="input"
-                  placeholder=""
-                  value={photoUrl}
-                  onChange={(event) => {
-                    setPhotoUrl(event.target.value);
-                  }}
-                />
-              </fieldset>
-              <fieldset className="fieldset mt-2">
-                <legend className="fieldset-legend"> Age: </legend>
-                <input
-                  type="text"
-                  className="input"
-                  placeholder=""
-                  value={age}
-                  onChange={(event) => {
-                    setAge(event.target.value);
-                  }}
-                />
-              </fieldset>
-              <fieldset className="fieldset mt-2">
-                <legend className="fieldset-legend"> Gender: </legend>
-                <input
-                  type="text"
-                  className="input"
-                  placeholder=""
-                  value={gender}
-                  onChange={(event) => {
-                    setGender(event.target.value);
-                  }}
-                />
-              </fieldset>
-              <fieldset className="fieldset mt-2">
-                <legend className="fieldset-legend"> About: </legend>
-                <input
-                  type="text"
-                  className="input"
-                  placeholder=""
-                  value={about}
-                  onChange={(event) => {
-                    setAbout(event.target.value);
-                  }}
-                />
-              </fieldset>
-            </div>
-            <p className="error-message  text-red-500">{errorMessage}</p>
-            <div className="card-actions justify-center mt-4">
-              <button className="btn btn-primary" onClick={saveProfile}>
-                Save Profile
-              </button>
+    <>
+      <div className="edit-profile-container  flex justify-center flex-wrap my-10">
+        <div className="card-container  flex justify-center mx-10">
+          {/* DaisyUI component - Card with no image */}
+          <div className="card bg-base-300 w-96 shadow-sm">
+            <div className="card-body">
+              <h2 className="card-title justify-center"> Edit Profile </h2>
+              <div>
+                {/* daisyui components - Input with fieldset and legend */}
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend"> First Name: </legend>
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder=""
+                    value={firstName}
+                    onChange={(event) => {
+                      setFirstName(event.target.value);
+                    }}
+                  />
+                </fieldset>
+                <fieldset className="fieldset mt-2">
+                  <legend className="fieldset-legend"> Last Name: </legend>
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder=""
+                    value={lastName}
+                    onChange={(event) => {
+                      setLastName(event.target.value);
+                    }}
+                  />
+                </fieldset>
+                <fieldset className="fieldset mt-2">
+                  <legend className="fieldset-legend"> Photo Url: </legend>
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder=""
+                    value={photoUrl}
+                    onChange={(event) => {
+                      setPhotoUrl(event.target.value);
+                    }}
+                  />
+                </fieldset>
+                <fieldset className="fieldset mt-2">
+                  <legend className="fieldset-legend"> Age: </legend>
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder=""
+                    value={age}
+                    onChange={(event) => {
+                      setAge(event.target.value);
+                    }}
+                  />
+                </fieldset>
+                <fieldset className="fieldset mt-2">
+                  <legend className="fieldset-legend"> Gender: </legend>
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder=""
+                    value={gender}
+                    onChange={(event) => {
+                      setGender(event.target.value);
+                    }}
+                  />
+                </fieldset>
+                <fieldset className="fieldset mt-2">
+                  <legend className="fieldset-legend"> About: </legend>
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder=""
+                    value={about}
+                    onChange={(event) => {
+                      setAbout(event.target.value);
+                    }}
+                  />
+                </fieldset>
+              </div>
+              <p className="error-message  text-red-500">{errorMessage}</p>
+              <div className="card-actions justify-center mt-4">
+                <button className="btn btn-primary" onClick={saveProfile}>
+                  Save Profile
+                </button>
+              </div>
             </div>
           </div>
         </div>
+
+        {/*  */}
+        <UserCard
+          user={{ firstName, lastName, photoUrl, age, gender, about }}
+        />
       </div>
 
-      {/*  */}
-      <UserCard user={{ firstName, lastName, photoUrl, age, gender, about }} />
-    </div>
+      {/*  daisyUI 'toast 'component - Profile Updated Success Msg UI */}
+      {showToast === true ? (
+        <div className="toast toast-top toast-center">
+          <div className="alert alert-success">
+            <span>Profile saved successfully.</span>
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
