76bddf55c5d68e2ec06dbdd4bec972d192d1cc77
Kumar <kumarnaidukarri2002@gmail.com>
2026-02-06 13:48:50 +0530

    12.3 - Applied 'Razorpay Payment Dialog Checkout Box opening' in Premium Component

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -12,8 +12,29 @@ const Premium = () => {
       { withCredentials: true },
     ); // API call to create an 'Order'
     console.log(order);
+    const { razorpayKeyId, amount, currency, notes, orderId } = order.data;
 
-    // It Should open the 'Razorpay Dialog Checkout Box'
+    // It should open the 'Razorpay Dialog Checkout Box'
+    const options = {
+      key: razorpayKeyId, // Replace with your Razorpay key_id
+      amount: amount / 100, // Amount is in currency subunits.
+      currency: currency,
+      name: "Dev tinder",
+      description: "Test Transactions",
+      order_id: orderId, // This is the order_id created in the backend
+      prefill: {
+        name: notes.firstName + " " + notes.lastName,
+        email: notes.emailId,
+        contact: "9999999999", // phone number to verify payment
+      },
+      theme: {
+        color: "#F37254",
+      },
+    };
+
+    const rzp = new window.Razorpay(options);
+    rzp.open(); // opens the razorpay dialog box
+    /* when react app loads, the razorpay js script file in HTML file will attach its 'Razorpay Object' to the "window" object */
   };
 
   return (
