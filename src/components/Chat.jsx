0924f7bdd3c97cb71ef579fd4a8661f6dd9fec19
Kumar <kumarnaidukarri2002@gmail.com>
2026-02-08 21:24:38 +0530

    13 - *** Building 'Realtime Chat Application' using 'Web Sockets' ***

---
+++ src/components/Chat.jsx
@@ -0,0 +1,20 @@
+// Chat.jsx componet contains 'Client Chat'
+
+const Chat = () => {
+  return <div> Chat Page </div>;
+};
+
+export default Chat;
+
+//
+/*
+Web Sockets :-
+ Web Sockets are a concept used to enable Real-time Communication. it is a Protocol.
+ Bi-Directional" communication: 2 way communication. i.e, Both Client and Server can send data at any time.
+ a server can connect with multiple clients and communicate each other.
+ Web socket connections can be established using: HTTP Long-Polling, WebSocket Protocol, WebTransport.
+
+"Socket.IO" Library :-
+ it is a JavaScript library built on top of 'Web Socket' and fallback techniques.
+ it enables 'low-latency', 'bi-directional', 'event-based' communication between client and server.
+*/
