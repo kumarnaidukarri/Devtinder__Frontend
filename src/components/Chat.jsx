3effeb63bba488d383a29c28bd7965322b55997a
Kumar <kumarnaidukarri2002@gmail.com>
2026-02-13 16:23:06 +0530

    14 - Added CHAT fetching (get last recent Chat from backend and show)

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,9 +3,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 // my modules
 import { createSocketConnection } from "../utils/socket.js";
+import { BASE_URL } from "../utils/constants.js";
 
 const Chat = () => {
   const params = useParams(); // 'useParams' Hook used to access 'Path Parameters' of URL.
@@ -15,6 +17,27 @@ const Chat = () => {
 
   const userData = useSelector((appStore) => appStore.user); // Subscribes to User slice of Redux store.
   const userId = userData?._id;
+
+  const fetchChatMessages = async () => {
+    // Get chat of the target user.
+    const chat = await axios.get(BASE_URL + "/chat/" + targetUserId, {
+      withCredentials: true,
+    });
+    console.log("target user chat - ", chat.data.messages);
+
+    const chatMessages = chat?.data?.messages.map((msg) => {
+      const { senderId, text } = msg;
+      return {
+        firstName: senderId?.firstName,
+        lastName: senderId?.lastName,
+        text: text,
+      };
+    });
+    setMessages(chatMessages); // Updates the state
+  };
+  useEffect(() => {
+    fetchChatMessages();
+  }, []);
 
   useEffect(() => {
     if (!userId) {
@@ -33,11 +56,11 @@ const Chat = () => {
     */
 
     // Socket Event - listen for incoming messages from backend . i.e, backend calls this event 'messageReceived'.
-    socket.on("messageReceived", ({ firstName, text }) => {
+    socket.on("messageReceived", ({ firstName, lastName, text }) => {
       console.log(
         "'messageReceived' event got Hitted in Socket Client(Frontend)",
       );
-      setMessages((messages) => [...messages, { firstName, text }]); // Updates the State
+      setMessages((messages) => [...messages, { firstName, lastName, text }]); // Updates the State
     });
 
     // *** Cleanup Func - when component unmount, Disconnect the Socket.
@@ -52,6 +75,7 @@ const Chat = () => {
     const socket = createSocketConnection(); // creates a socket client connection.
     socket.emit("sendMessage", {
       firstName: userData.firstName,
+      lastName: userData.lastName,
       userId,
       targetUserId,
       text: newMessage,
@@ -75,12 +99,30 @@ const Chat = () => {
           */
           // Daisy UI component - Chat Bubble(chat with header footer)
           return (
-            <div key={index} className="chat chat-start">
+            // my messages must be on 'right-side' and receiver user messages on 'left-side'.
+            <div
+              key={index}
+              className={
+                "chat " +
+                (msg.firstName === userData.firstName
+                  ? "chat-end"
+                  : "chat-start")
+              }
+            >
               <div className="chat-header">
-                {msg.firstName}
+                {msg.firstName + " " + msg.lastName}
                 <time className="text-xs opacity-50">2 hours ago</time>
               </div>
-              <div className="chat-bubble">{msg.text}</div>
+              <div
+                className={
+                  "chat-bubble " +
+                  (msg.firstName === userData.firstName
+                    ? "bg-[#075e54]"
+                    : "bg-gray")
+                }
+              >
+                {msg.text}
+              </div>
               <div className="chat-footer opacity-50">Seen</div>
             </div>
           );
