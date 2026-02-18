9263767f07629ad8d7af5b9ef3cb43d6ac63d569
Kumar <kumarnaidukarri2002@gmail.com>
2026-02-18 23:49:09 +0530

    Config Frontend for deployment in 'VERCEL' hosting platform

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,14 @@
 // constants.js file contains all hard coded urls, strings.
 
-const BASE_URL = "http://localhost:3000";
+// const BASE_URL = "http://localhost:3000";
+// const BASE_URL = "https://devtinder-backend-x5rv.onrender.com";
+
+let BASE_URL;
+
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+  BASE_URL = import.meta.env.VITE_LOCAL_BACKEND_URL; // Local dev backend url
+} else {
+  BASE_URL = import.meta.env.VITE_DEPLOYED_BACKEND_URL; // Deployed backend url
+}
 
 export { BASE_URL };
